f3a8eb4e3e8bdd70eddf3633df769c5519b94453
Mark <mark.balazon@gmail.com>
2022-01-25 19:41:01 -0500

    Schedule section boilerplate

--- components/Schedule/Schedule.js
+++ components/Schedule/Schedule.js
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h2`
+  font-size: 150px;
+  font-weight: normal;
+  height: auto;
+  letter-spacing: -0.05em;
+  margin: 0px;
+`;
 
 function Schedule() {
-  return <div></div>;
+  return (
+    <div>
+      <Title>Schedule</Title>
+    </div>
+  );
 }
 
 export default Schedule;
